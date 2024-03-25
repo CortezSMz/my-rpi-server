00f88f07f46066572e14f043773c22ab81ef2167
CortezSMz <cortez_alexandre@hotmail.com>
2024-03-25 12:00:33 -0300

    refactor(mosquitto): separated system info messages per topic

--- mosquitto/publish.sh
+++ mosquitto/publish.sh
@@ -1,51 +1,39 @@
 #!/bin/bash
 source ./../.env
 
-getMessage() {
-    read -r -d '' MSG <<- EOM
-        {
-            "uptime": "`uptime | awk -F "up|\," '{print $2}'`",
-            "cpu": {
-                "temperature": `cat /sys/class/thermal/thermal_zone0/temp`,
-                "usage": {
-                    "cpu0": "TODO",
-                    "cpu1": "TODO",
-                    "cpu2": "TODO",
-                    "cpu3": "TODO"
-                },
-                "clock_frequency": {
-                    "min": `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq`,
-                    "max": `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq`,
-                    "current": `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq`
-                },
-                "load_average": {
-                    "1min":  `cat /proc/loadavg | awk '{print $1}'`,
-                    "5min":  `cat /proc/loadavg | awk '{print $2}'`,
-                    "15min": `cat /proc/loadavg | awk '{print $3}'`
-                }
-            },
-            "memory": {
-                "total": `free -m | awk '/^Mem:/ {print $2}'`,
-                "used": `free -m | awk '/^Mem:/ {print $3}'`,
-                "used_percentage": `free | awk '/^Mem:/ {printf "%.1f", ($3/$2) * 100}'`,
-                "available": `free -m | awk '/^Mem:/ {print $7}'`
-            },
-            "disk": {
-                "total": "`df -BM | awk '/^\/dev\// {total = $2; used = $3; available = $4} END {print total}'`",
-                "used": "`df -BM | awk '/^\/dev\// {total = $2; used = $3; available = $4} END {print used}'`",
-                "available": "`df -BM | awk '/^\/dev\// {total = $2; used = $3; available = $4} END {print available}'`",
-                "used_percentage": `df -BM | awk '/^\/dev\// {total = $2; used = $3; available = $4} END {printf "%.1f", (used / (total + used)) * 100}'`
-            }
-        }
-EOM
+publish() {
+    TOPIC=$1
+    shift;
+    MSG=$@
+    CMD="mosquitto_pub --retain -h localhost -u $MQTT_USER -P $MQTT_PASS -t system/$TOPIC -m $MSG"
+    $CMD
 }
 
 while [ true ]
 do
-    getMessage
+    publish uptime "`uptime | awk -F "up|\," '{print $2}' | sed 's/\s//g'`"
 
-    CMD="mosquitto_pub -h localhost -u $MQTT_USER -P $MQTT_PASS -t system/info -m $(echo $MSG | sed -r 's/[[:space:]]*//g' )"
-    $CMD
+    publish cpu/temperature `cat /sys/class/thermal/thermal_zone0/temp`
+
+    publish cpu/usage/all `mpstat -P ALL -u | grep -E '\sall\s' | awk '{print $3}'`
+    publish cpu/usage/0 `mpstat -P 0 -u | grep -E '\s0\s' | awk '{print $3}'`
+    publish cpu/usage/1 `mpstat -P 1 -u | grep -E '\s1\s' | awk '{print $3}'`
+    publish cpu/usage/2 `mpstat -P 2 -u | grep -E '\s2\s' | awk '{print $3}'`
+    publish cpu/usage/3 `mpstat -P 3 -u | grep -E '\s3\s' | awk '{print $3}'`
+
+    publish cpu/loadavg/1min  `cat /proc/loadavg | awk '{print $1}'`
+    publish cpu/loadavg/5min  `cat /proc/loadavg | awk '{print $2}'`
+    publish cpu/loadavg/15min `cat /proc/loadavg | awk '{print $3}'`
+    
+    publish memory/total `free -m | awk '/^Mem:/ {print $2}'`
+    publish memory/used `free -m | awk '/^Mem:/ {print $3}'`
+    publish memory/available `free -m | awk '/^Mem:/ {print $7}'`
+    publish memory/used_percentage `free | awk '/^Mem:/ {printf "%.1f", ($3/$2) * 100}'`
+
+    publish disk/total `df -k | awk '/^\/dev\/sda2/ {total = $2} END {print total}'`
+    publish disk/used `df -k | awk '/^\/dev\/sda2/ {used = $3} END {print used}'`
+    publish disk/available `df -k | awk '/^\/dev\/sda2/ {available = $4} END {print available}'`
+    publish disk/used_percentage `df -k | awk '/^\/dev\/sda2/ {used_percentage = $5} END {print used_percentage}' | sed 's/%//'`
 
     sleep 5
 done
